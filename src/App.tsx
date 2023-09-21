bab519f4c573bf37b3ac204450d872e320df231b
AlexeyLavrishin <alexeylavrishin@gmail.com>
2023-09-21 09:48:59 +0300

    feat: add handle for new task item (by click and press Enter)

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,38 @@ import React, {useState} from 'react';
 import './App.css';
 import TodoList, {TaskType} from "./TodoList";
 import {FilterType} from "./CommonTypes/FilterType";
-import {AdditionalTaskTodoListApp1} from "./AdditionalTasksTodoList/AdditionalTaskTodoListApp1";
-
+import {v1} from "uuid";
 
 
 function App() {
     // const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     const initialState = [
-        {id: 1, title: "SocialNetwork", isDone: false},
-        {id: 2, title: "TodoList", isDone: false},
-        {id: 3, title: "MicroTask", isDone: true},
+        {id: v1(), title: "SocialNetwork", isDone: false},
+        {id: v1(), title: "TodoList", isDone: false},
+        {id: v1(), title: "MicroTask", isDone: true},
     ]
     const [tasks, setTasks] = useState<Array<TaskType>>(initialState);
     const [filterValue, setFilter] = useState<FilterType>(FilterType.All)
 
-    function handleDeleteTask(id: number) {
+    function onDeleteTaskItem(id: string) {
         setTasks(tasks.filter(task => task.id !== id))
     }
 
-    function handleFilterTasks(filterValue: FilterType) {
+    function onAddTaskItem(taskItem: string) {
+        const newTaskItem = {id: v1(), title: taskItem, isDone: false};
+        setTasks([newTaskItem, ...tasks])
+    }
+
+    function onFilterTasks(filterValue: FilterType) {
         setFilter(filterValue)
     }
+
     let tasksForTodoList = tasks;
     if (filterValue === "active") {
-        tasksForTodoList = tasks.filter(task => task.isDone === false)
+        tasksForTodoList = tasks.filter(task => !task.isDone)
     }
     if (filterValue === "completed") {
-        tasksForTodoList = tasks.filter(task => task.isDone === true)
+        tasksForTodoList = tasks.filter(task => task.isDone)
     }
 
 
@@ -36,13 +41,12 @@ function App() {
         <div className="App">
             <TodoList title={"Monday"}
                       tasks={tasksForTodoList}
-                      onDeleteTask={handleDeleteTask}
-                      onFilterTasks={handleFilterTasks}/>
+                      onDeleteTaskItem={onDeleteTaskItem}
+                      onFilterTasks={onFilterTasks}
+                      onAddTaskItem={onAddTaskItem}/>
 
-            {/*<AdditionalTaskTodoListApp1/>*/}
         </div>
     );
 }
 
 export default App;
-// +WTB DRAGONSKULL куски и рецепт(key, recipe)
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,40 +1,75 @@
-import React, {FC} from "react";
+import React, {ChangeEvent, KeyboardEvent, FC, useState} from "react";
 import {FilterType} from "./CommonTypes/FilterType";
 
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
+
 type TodoListPropsType = {
     title: string
     tasks: Array<TaskType>
-    onDeleteTask: (id: number) => void
-    onFilterTasks:(value: FilterType) => void
+    onDeleteTaskItem: (id: string) => void
+    onFilterTasks: (value: FilterType) => void
+    onAddTaskItem: (taskItem: string) => void
 }
 
 const TodoList: FC<TodoListPropsType> = (props) => {
+    const [taskItem, setTaskItem] = useState("");
+
+    function handleTaskListInput(e: ChangeEvent<HTMLInputElement>) {
+        setTaskItem(e.currentTarget.value)
+    }
+
+    function handleAddTaskItem() {
+        props.onAddTaskItem(taskItem);
+        setTaskItem("")
+    }
+
+    function handleAddTaskItemOnPressEnter(e: KeyboardEvent<HTMLInputElement>) {
+        e.key === "Enter" && handleAddTaskItem();
+    }
+
+    function handleTaskFilterAll() {
+        props.onFilterTasks(FilterType.All)
+    }
+
+    function handleTaskFilterActive() {
+        props.onFilterTasks(FilterType.Active)
+    }
+
+    function handleTaskFilterCompleted() {
+        props.onFilterTasks(FilterType.Completed)
+    }
+
+    const tasksItems = props.tasks.map(task => {
+            const onDeleteTask = () => props.onDeleteTaskItem(task.id)
+            return (
+                <li key={task.id}>
+                    <input type="checkbox" checked={task.isDone}/>
+                    <span>{task.title}</span>
+                    <button onClick={onDeleteTask}>&otimes;</button>
+                </li>
+            )
+        }
+    )
+
 
     return (
         <div>
             <h3>{props.title}</h3>
             <div>
-                <input/>
-                <button>+</button>
+                <input value={taskItem}
+                       onChange={handleTaskListInput}
+                       onKeyDown={handleAddTaskItemOnPressEnter}/>
+                <button onClick={handleAddTaskItem}>+</button>
             </div>
-            <ul>
-                {props.tasks.map(task => (
-                    <li key={task.id}>
-                        <input type="checkbox" checked={task.isDone}/>
-                        <span>{task.title}</span>
-                        <button onClick={() => props.onDeleteTask(task.id)}>&otimes;</button>
-                    </li>
-                ))}
-            </ul>
+            <ul>{tasksItems}</ul>
             <div>
-                <button onClick={() => props.onFilterTasks(FilterType.All)}>All</button>
-                <button onClick={() => props.onFilterTasks(FilterType.Active)}>Active</button>
-                <button onClick={() => props.onFilterTasks(FilterType.Completed)}>Completed</button>
+                <button onClick={handleTaskFilterAll}>All</button>
+                <button onClick={handleTaskFilterActive}>Active</button>
+                <button onClick={handleTaskFilterCompleted}>Completed</button>
             </div>
         </div>
     );
