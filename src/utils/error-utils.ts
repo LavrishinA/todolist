effaf11fe2cae6a7a1a4510fc1d315dd24e5feb1
AlexeyLavrishin <alexeylavrishin@gmail.com>
2024-01-15 16:10:56 +0300

    ref: task-slice, rewrite async thunk with createAsyncThunk

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,6 +1,7 @@
 import { TodolistResponse } from "api/todolistApi"
 import { appActions } from "app/app-slice"
 import { AppDispatch } from "app/store"
+import axios from "axios"
 
 export const handleServerAppError = (data: TodolistResponse, dispatch: AppDispatch) => {
     if (data.messages.length) {
@@ -11,7 +12,22 @@ export const handleServerAppError = (data: TodolistResponse, dispatch: AppDispat
     dispatch(appActions.setStatus({ status: "failed" }))
 }
 
-export const handleServerNetworkError = (error: { message: string }, dispatch: AppDispatch) => {
-    dispatch(appActions.setError({ error: error.message }))
+export const handleServerNetworkError = (err: unknown, dispatch: AppDispatch): void => {
+    let errorMessage = "Some error occurred"
+
+    // ❗Проверка на наличие axios ошибки
+    if (axios.isAxiosError(err)) {
+        // ⏺️ err.response?.data?.message - например получение тасок с невалидной todolistId
+        // ⏺️ err?.message - например при создании таски в offline режиме
+        errorMessage = err.response?.data?.message || err?.message || errorMessage
+        // ❗ Проверка на наличие нативной ошибки
+    } else if (err instanceof Error) {
+        errorMessage = `Native error: ${err.message}`
+        // ❗Какой-то непонятный кейс
+    } else {
+        errorMessage = JSON.stringify(err)
+    }
+
+    dispatch(appActions.setError({ error: errorMessage }))
     dispatch(appActions.setStatus({ status: "failed" }))
 }
